bd946907001ee83a13c16b0a920956d69becb4b0
Koert van der Veer <koert@openpanel.com>
2010-11-10 10:51:33 +0100

    Fixed commenting style.

--- apache2Module.h
+++ apache2Module.h
@@ -1,11 +1,11 @@
-# This file is part of OpenPanel - The Open Source Control Panel
-# OpenPanel is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License as published by the Free 
-# Software Foundation, using version 3 of the License.
-#
-# Please note that use of the OpenPanel trademark may be subject to additional 
-# restrictions. For more information, please visit the Legal Information 
-# section of the OpenPanel website on http://www.openpanel.com/
+// This file is part of OpenPanel - The Open Source Control Panel
+// OpenPanel is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the Free 
+// Software Foundation, using version 3 of the License.
+//
+// Please note that use of the OpenPanel trademark may be subject to additional 
+// restrictions. For more information, please visit the Legal Information 
+// section of the OpenPanel website on http://www.openpanel.com/
 
 #ifndef _networkingModule_H
 #define _networkingModule_H 1
