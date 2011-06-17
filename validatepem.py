1a8e5eb57d1bee4de61df68c0ce63e88c358ff54
Peter van Dijk <peter@openpanel.com>
2011-06-16 17:18:50 -0700

    validatepem: verify that there is exactly one key; check that at least one cert matches with the key

--- validatepem.py
+++ validatepem.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python
 import sys, subprocess, os
 
+certs=[]
+keys=[]
+
 def openssl(cmd, input):
-	p = subprocess.Popen(["/usr/bin/openssl"] + cmd, stdin=subprocess.PIPE, stderr=open(os.devnull,'w'))
+	p = subprocess.Popen(["/usr/bin/openssl"] + cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=open(os.devnull,'w'))
 	p.stdin.write('\n'.join(input))
 	p.stdin.close()
+	output = p.stdout.read()
 	if p.wait():
 		sys.exit(1)
+	return output
 
 def verifycert(cert):
-	openssl(['x509','-noout'], cert)
+	global certs
+	modulus = openssl(['x509','-noout','-modulus'], cert)
+	certs.append(modulus)
 
 def verifykey(key):
-	openssl(['rsa','-noout', '-passin','pass:'], key)
+	global keys
+	modulus = openssl(['rsa','-noout', '-passin','pass:','-modulus'], key)
+	keys.append(modulus)
 
 def tryverify(pem):
 	if pem and pem[0] == "-----BEGIN CERTIFICATE-----":
@@ -22,8 +31,6 @@ def tryverify(pem):
 
 f = open(sys.argv[1])
 
-certs=[]
-keys=[]
 
 cur=[]
 for l in f:
@@ -35,3 +42,12 @@ for l in f:
 	cur.append(l)
 
 tryverify(cur)
+
+if not certs:
+	sys.exit(1)
+
+if len(keys) != 1:
+	sys.exit(1)
+
+if keys[0] not in certs:
+	sys.exit(1)
